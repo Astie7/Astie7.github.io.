4c1de2d63da584d044e5e11a5f5e8ae138567519
Astie <astie@example.com>
2026-02-07 17:04:00 +0200

    Force signup redirect to live rules URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,5 @@
 window.PV_SUPABASE = window.PV_SUPABASE || {
     url: "https://hdchgfqusjwloxuzaebv.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkY2hnZnF1c2p3bG94dXphZWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0MTA3MjMsImV4cCI6MjA4NTk4NjcyM30.XofJWA76O8aeV7UqY7G2HGtdjdzB1i6YpKFRqkGcHcU"
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkY2hnZnF1c2p3bG94dXphZWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0MTA3MjMsImV4cCI6MjA4NTk4NjcyM30.XofJWA76O8aeV7UqY7G2HGtdjdzB1i6YpKFRqkGcHcU",
+    authRedirectTo: "https://astie7.github.io/Pantheverse/rules/"
 };
